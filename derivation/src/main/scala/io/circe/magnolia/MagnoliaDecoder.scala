886a332089923f445f5d69c496ba30435b6bf5bc
Samuel Nelson <samuel.nelson@elastic.co>
2022-08-04 17:57:52 +1200

    Implement error accumulation

--- derivation/src/main/scala/io/circe/magnolia/MagnoliaDecoder.scala
+++ derivation/src/main/scala/io/circe/magnolia/MagnoliaDecoder.scala
@@ -1,8 +1,10 @@
 package io.circe.magnolia
 
+import cats.data.{NonEmptyList, Validated}
+import cats.data.Validated.{Invalid, Valid}
 import cats.syntax.either._
 import io.circe.{Decoder, DecodingFailure, HCursor}
-import io.circe.Decoder.Result
+import io.circe.Decoder.{AccumulatingResult, Result}
 import io.circe.magnolia.configured.Configuration
 import magnolia._
 
@@ -11,7 +13,7 @@ private[magnolia] object MagnoliaDecoder {
   private[magnolia] def combine[T](
     caseClass: CaseClass[Decoder, T]
   )(implicit configuration: Configuration): Decoder[T] = {
-    val paramJsonKeyLookup: Map[String, String] = caseClass.parameters.map{ p =>
+    val paramJsonKeyLookup: Map[String, String] = caseClass.parameters.map { p =>
       val jsonKeyAnnotation = p.annotations.collectFirst {
         case ann: JsonKey => ann
       }
@@ -22,40 +24,72 @@ private[magnolia] object MagnoliaDecoder {
     }.toMap
 
     if (paramJsonKeyLookup.values.toList.distinct.length != caseClass.parameters.length) {
-      throw new DerivationError("Duplicate key detected after applying transformation function for case class parameters")
+      throw new DerivationError(
+        "Duplicate key detected after applying transformation function for case class parameters"
+      )
     }
 
     val nonStrictDecoder = if (configuration.useDefaults) {
       new Decoder[T] {
         override def apply(c: HCursor): Result[T] = {
+          decodeAccumulating(c).toEither.leftMap(_.head)
+        }
+
+        override def decodeAccumulating(c: HCursor): AccumulatingResult[T] = {
           caseClass.constructEither { p =>
-            val key = paramJsonKeyLookup.getOrElse(p.label, throw new IllegalStateException("Looking up a parameter label should always yield a value. This is a bug"))
+            val key = paramJsonKeyLookup.getOrElse(
+              p.label,
+              throw new IllegalStateException(
+                "Looking up a parameter label should always yield a value. This is a bug"
+              )
+            )
             val keyCursor = c.downField(key)
             keyCursor.focus match {
               case Some(_) => p.typeclass.tryDecode(keyCursor)
-              case None => p.default.fold {
-                // Some decoders (in particular, the default Option[T] decoder) do special things when a key is missing,
-                // so we give them a chance to do their thing here.
-                p.typeclass.tryDecode(keyCursor)
-              }(x => Right(x))
+              case None =>
+                p.default.fold {
+                  // Some decoders (in particular, the default Option[T] decoder) do special things when a key is missing,
+                  // so we give them a chance to do their thing here.
+                  p.typeclass.tryDecode(keyCursor)
+                }(x => Right(x))
             }
-          }.leftMap(_.head)
+          } match {
+            case Right(a) => Valid(a)
+            case Left(head :: tail) => Invalid(NonEmptyList(head, tail))
+            case Left(Nil) => throw new IllegalStateException("Decoding failed, but no errors returned. This is a bug")
+          }
         }
       }
-    }
-    else {
+    } else {
       new Decoder[T] {
-        def apply(c: HCursor): Result[T] = {
+        override def apply(c: HCursor): Result[T] = {
+          decodeAccumulating(c).toEither.leftMap(_.head)
+        }
+
+        override def decodeAccumulating(c: HCursor): AccumulatingResult[T] = {
           caseClass.constructEither { p =>
-            p.typeclass.tryDecode(c.downField(paramJsonKeyLookup.getOrElse(p.label, throw new IllegalStateException("Looking up a parameter label should always yield a value. This is a bug"))))
+            p.typeclass.tryDecode(
+              c.downField(
+                paramJsonKeyLookup.getOrElse(
+                  p.label,
+                  throw new IllegalStateException(
+                    "Looking up a parameter label should always yield a value. This is a bug"
+                  )
+                )
+              )
+            )
+          } match {
+            case Right(a) => Valid(a)
+            case Left(head :: tail) => Invalid(NonEmptyList(head, tail))
+            case Left(Nil) => throw new IllegalStateException("Decoding failed, but no errors returned. This is a bug")
           }
-        }.leftMap(_.head)
+        }
       }
     }
 
     if (configuration.strictDecoding) {
       val expectedFields = paramJsonKeyLookup.values
-      val strictDecoder = nonStrictDecoder.validate { cursor: HCursor =>
+      nonStrictDecoder.validate { cursor: HCursor =>
         val maybeUnexpectedErrors = for {
           json <- cursor.focus
           jsonKeys <- json.hcursor.keys
@@ -68,7 +102,6 @@ private[magnolia] object MagnoliaDecoder {
 
         maybeUnexpectedErrors.getOrElse(List("Couldn't determine decoded fields."))
       }
-      (c: HCursor) => strictDecoder(c)
     } else {
       nonStrictDecoder
     }
@@ -77,14 +110,15 @@ private[magnolia] object MagnoliaDecoder {
   private[magnolia] def dispatch[T](
     sealedTrait: SealedTrait[Decoder, T]
   )(implicit configuration: Configuration): Decoder[T] = {
-
     val constructorLookup: Map[String, Subtype[Decoder, T]] =
       sealedTrait.subtypes.map { s =>
         configuration.transformConstructorNames(s.typeName.short) -> s
       }.toMap
 
     if (constructorLookup.size != sealedTrait.subtypes.length) {
-      throw new DerivationError("Duplicate key detected after applying transformation function for case class parameters")
+      throw new DerivationError(
+        "Duplicate key detected after applying transformation function for case class parameters"
+      )
     }
 
     configuration.discriminator match {
@@ -93,68 +127,112 @@ private[magnolia] object MagnoliaDecoder {
     }
   }
 
-  private[magnolia] class NonDiscriminatedDecoder[T](constructorLookup: Map[String, Subtype[Decoder, T]]) extends Decoder[T] {
+  private[magnolia] class NonDiscriminatedDecoder[T](constructorLookup: Map[String, Subtype[Decoder, T]])
+    extends Decoder[T] {
     private val knownSubTypes = constructorLookup.keys.toSeq.sorted.mkString(",")
 
     override def apply(c: HCursor): Result[T] = {
-      c.keys match {
-            case Some(keys) if keys.size == 1 =>
-              val key = keys.head
-              for {
-                theSubtype <- Either.fromOption(
-                  constructorLookup.get(key),
-                  DecodingFailure(
-                    s"""Can't decode coproduct type: couldn't find matching subtype.
-                       |JSON: ${c.value},
-                       |Key: $key
-                       |Known subtypes: $knownSubTypes\n""".stripMargin,
-                    c.history
-                  )
-                )
+      decodeAccumulating(c).toEither.leftMap(_.head)
+    }
 
-                result <- c.get(key)(theSubtype.typeclass)
-              } yield result
-            case _ =>
-              Left(
+    override def decodeAccumulating(c: HCursor): AccumulatingResult[T] = {
+      c.keys match {
+        case Some(keys) if keys.size == 1 =>
+          val key = keys.head
+          constructorLookup.get(key) match {
+            case Some(theSubtype) =>
+              theSubtype.typeclass.tryDecodeAccumulating(c.downField(key))
+            case None =>
+              Validated.invalidNel(
                 DecodingFailure(
-                  s"""Can't decode coproduct type: zero or several keys were found, while coproduct type requires exactly one.
+                  s"""Can't decode coproduct type: couldn't find matching subtype.
                    |JSON: ${c.value},
-                   |Keys: ${c.keys.map(_.mkString(","))}
+                   |Key: $key
                    |Known subtypes: $knownSubTypes\n""".stripMargin,
                   c.history
                 )
               )
           }
+        case _ =>
+          Validated.invalidNel(
+            DecodingFailure(
+              s"""Can't decode coproduct type: zero or several keys were found, while coproduct type requires exactly one.
+                 |JSON: ${c.value},
+                 |Keys: ${c.keys.map(_.mkString(","))}
+                 |Known subtypes: $knownSubTypes\n""".stripMargin,
+              c.history
+            )
+          )
+      }
     }
   }
 
-  private[magnolia] class DiscriminatedDecoder[T](discriminator: String, constructorLookup: Map[String, Subtype[Decoder, T]]) extends Decoder[T] {
+  private[magnolia] class DiscriminatedDecoder[T](
+    discriminator: String,
+    constructorLookup: Map[String, Subtype[Decoder, T]]
+  ) extends Decoder[T] {
     val knownSubTypes = constructorLookup.keys.toSeq.sorted.mkString(",")
 
     override def apply(c: HCursor): Result[T] = {
       c.downField(discriminator).as[String] match {
-        case Left(_) => Left(DecodingFailure(
-          s"""
+        case Left(_) =>
+          Left(
+            DecodingFailure(
+              s"""
              |Can't decode coproduct type: couldn't find discriminator or is not of type String.
              |JSON: ${c.value}
              |discriminator key: discriminator
               """.stripMargin,
-          c.history
-        ))
-        case Right(ctorName) => constructorLookup.get(ctorName) match {
-          case Some(subType) => subType.typeclass.apply(c)
-          case None => Left(DecodingFailure(
-            s"""
+              c.history
+            )
+          )
+        case Right(ctorName) =>
+          constructorLookup.get(ctorName) match {
+            case Some(subType) => subType.typeclass.apply(c)
+            case None =>
+              Left(
+                DecodingFailure(
+                  s"""
                |Can't decode coproduct type: constructor name not found in known constructor names
                |JSON: ${c.value}
                |Allowed discriminators: $knownSubTypes
               """.stripMargin,
-            c.history
-          ))
-        }
+                  c.history
+                )
+              )
+          }
       }
     }
 
+    override def decodeAccumulating(c: HCursor): AccumulatingResult[T] = {
+      c.downField(discriminator).as[String] match {
+        case Left(_) =>
+          Validated.invalidNel(
+            DecodingFailure(
+              s"""
+                 |Can't decode coproduct type: couldn't find discriminator or is not of type String.
+                 |JSON: ${c.value}
+                 |discriminator key: discriminator
+              """.stripMargin,
+              c.history
+            )
+          )
+        case Right(ctorName) =>
+          constructorLookup.get(ctorName) match {
+            case Some(subType) => subType.typeclass.tryDecodeAccumulating(c)
+            case None =>
+              Validated.invalidNel(
+                DecodingFailure(
+                  s"""
+                     |Can't decode coproduct type: constructor name not found in known constructor names
+                     |JSON: ${c.value}
+                     |Allowed discriminators: $knownSubTypes
+              """.stripMargin,
+                  c.history
+                )
+              )
+          }
+      }
+    }
   }
 }
-
